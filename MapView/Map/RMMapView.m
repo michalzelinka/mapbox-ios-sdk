46c52fe0ac0f34854207ddeb83e834f69b69fba9
Michal Zelinka <michal.zelinka@twobits.cz>
2014-03-28 11:22:52 +0100

    Disabled initialisation of Compass view

--- MapView/Map/RMMapView.m
+++ MapView/Map/RMMapView.m
@@ -313,19 +313,21 @@
 
     self.showLogoBug = YES;
 
-    if (RMPostVersion7)
-    {
-        _compassButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        UIImage *compassImage = [RMMapView resourceImageNamed:@"Compass.png"];
-        _compassButton.frame = CGRectMake(0, 0, compassImage.size.width, compassImage.size.height);
-        [_compassButton setImage:compassImage forState:UIControlStateNormal];
-        _compassButton.alpha = 0;
-        [_compassButton addTarget:self action:@selector(tappedHeadingCompass:) forControlEvents:UIControlEventTouchUpInside];
-        UIView *container = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width - compassImage.size.width - 5, 5, compassImage.size.width, compassImage.size.height)];
-        container.translatesAutoresizingMaskIntoConstraints = NO;
-        [container addSubview:_compassButton];
-        [self addSubview:container];
-    }
+// Tripomatic: Disable creation of Compass view as we use our own
+//
+//    if (RMPostVersion7)
+//    {
+//        _compassButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//        UIImage *compassImage = [RMMapView resourceImageNamed:@"Compass.png"];
+//        _compassButton.frame = CGRectMake(0, 0, compassImage.size.width, compassImage.size.height);
+//        [_compassButton setImage:compassImage forState:UIControlStateNormal];
+//        _compassButton.alpha = 0;
+//        [_compassButton addTarget:self action:@selector(tappedHeadingCompass:) forControlEvents:UIControlEventTouchUpInside];
+//        UIView *container = [[UIView alloc] initWithFrame:CGRectMake(self.bounds.size.width - compassImage.size.width - 5, 5, compassImage.size.width, compassImage.size.height)];
+//        container.translatesAutoresizingMaskIntoConstraints = NO;
+//        [container addSubview:_compassButton];
+//        [self addSubview:container];
+//    }
 
     self.displayHeadingCalibration = YES;
 
